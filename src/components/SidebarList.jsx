efaedab4215f3c511e3b4c1778e1d974b8a6fba6
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-11-13 11:58:45 +0330

    update

--- src/components/SidebarList.jsx
+++ src/components/SidebarList.jsx
@@ -15,23 +15,15 @@ const SidebarList = () => {
                 : dispatch({ type: "DMOBILE" })
             }
           >
-            <Link to="/mobile" className="link" >
+            <Link
+              to="/mobile"
+              className="link"
+              onClick={() => dispatch({ type: "HIDEN" })}
+            >
               <h3> Mobile</h3>
             </Link>
-            <i
-              className={`fa-solid opacty fa-${
-                state.showMobile === "actv" ? "plus" : "minus"
-              }`}
-            ></i>
+            <i class="fa-solid fa-arrow-left"></i>
           </li>
-          {/* <ul className={` ul ${state.showMobile}`}>
-            <li>Appel</li>
-            <li>Samsung</li>
-            <li>Lg</li>
-            <li>Xiaomi</li>
-            <li>Motorola</li>
-          </ul> */}
-
           <li
             className="list"
             onClick={() =>
@@ -40,22 +32,16 @@ const SidebarList = () => {
                 : dispatch("DLABTAB")
             }
           >
-            <Link to="/labtab" className="link">
+            <Link
+              to="/labtab"
+              className="link"
+              onClick={() => dispatch({ type: "HIDEN" })}
+            >
               <h3>Labtab</h3>
             </Link>
-            <i
-              className={`fa-solid opacty fa-${
-                state.showLabtab === "actv" ? "plus" : "minus"
-              }`}
-            ></i>
+            <i class="fa-solid fa-arrow-left"></i>
           </li>
-          <ul className={`ul ${state.showLabtab}`}>
-            <li>Asus</li>
-            <li>Samsung</li>
-            <li>Appel</li>
-            <li>Acer</li>
-            <li>Lonovo</li>
-          </ul>
+
           <li
             className="list"
             onClick={() =>
@@ -64,65 +50,47 @@ const SidebarList = () => {
                 : dispatch("DFLASH")
             }
           >
-            <Link to="/Flash Memory" className="link">
+            <Link
+              to="/Flash Memory"
+              className="link"
+              onClick={() => dispatch({ type: "HIDEN" })}
+            >
               <h3> Flash</h3>
             </Link>
-            <i
-              className={`fa-solid opacty fa-${
-                state.showFlash === "actv" ? "plus" : "minus"
-              }`}
-            ></i>
+            <i class="fa-solid fa-arrow-left"></i>
           </li>
-          <ul className={`ul ${state.showFlash}`}>
-            <li>Adata</li>
-            <li>Samsung</li>
-            <li>Sandisk</li>
-            <li>Hp</li>
-            <li>KingStone</li>
-          </ul>
+
           <li
             className="list"
             onClick={() =>
               state.showHdd === "actv" ? dispatch("HDD") : dispatch("DHDD")
             }
           >
-            <Link to="/Hdd Disk" className="link">
+            <Link
+              to="/Hdd Disk"
+              className="link"
+              onClick={() => dispatch({ type: "HIDEN" })}
+            >
               <h3> Hdd</h3>
             </Link>
-            <i
-              className={`fa-solid opacty fa-${
-                state.showHdd === "actv" ? "plus" : "minus"
-              }`}
-            ></i>
+            <i class="fa-solid fa-arrow-left"></i>
           </li>
-          <ul className={`ul ${state.showHdd}`}>
-            <li>WestrenDigital</li>
-            <li>Samsung</li>
-            <li>Seget</li>
-            <li>Maxtor</li>
-          </ul>
+
           <li
             className="list"
             onClick={() =>
               state.showSdd === "actv" ? dispatch("SDD") : dispatch("DSDD")
             }
           >
-            <Link to="/Sdd Disk" className="link">
+            <Link
+              to="/Sdd Disk"
+              className="link"
+              onClick={() => dispatch({ type: "HIDEN" })}
+            >
               <h3> Sdd</h3>
             </Link>
-            <i
-              className={`fa-solid opacty fa-${
-                state.showSdd === "actv" ? "plus" : "minus"
-              }`}
-            ></i>
+            <i class="fa-solid fa-arrow-left"></i>
           </li>
-          <ul className={`ul ${state.showSdd}`}>
-            <li>Adata</li>
-            <li>Samsung</li>
-            <li>Sandisk</li>
-            <li>Acer</li>
-            <li>Asus</li>
-          </ul>
         </ul>
       </div>
     );
