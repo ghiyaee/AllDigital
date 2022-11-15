b977576b5bba841a2c8a8ea61572c30aff4fe7bc
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-11-15 15:30:24 +0330

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,17 @@ const Footer = () => {
   return (
     <div className="container-footer">
       {/* <div className="imgser">
-        <img className="imgser" src={imgfooter[0]} alt="" />
+       
         <img className="imgser" src={imgfooter[1]} alt="" />
         <img className="imgser" src={imgfooter[2]} alt="" />
       </div> */}
       <div className="footer-info">
         <ul className="footer-header-ul">
           <li className="footer-header-li">
-            <h2>salary</h2>
+            <div className="headfooter">
+              <h2>salary</h2>
+              <img className="imgser" src={imgfooter[0]} alt="" />
+            </div>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. In
               incidunt fuga quas totam officia maiores ipsum dolor reiciendis
@@ -25,7 +28,10 @@ const Footer = () => {
             </p>
           </li>
           <li className="footer-header-li">
-            <h2>support</h2>
+            <div className="headfooter">
+              <h2>support</h2>
+              <img className="imgser" src={imgfooter[1]} alt="" />
+            </div>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. In
               incidunt fuga quas totam officia maiores ipsum dolor reiciendis
@@ -36,7 +42,10 @@ const Footer = () => {
             </p>
           </li>
           <li className="footer-header-li">
-            <h2>about us</h2>
+            <div className="headfooter">
+              <h2>about us</h2>
+              <img className="imgser" src={imgfooter[2]} alt="" />
+            </div>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. In
               incidunt fuga quas totam officia maiores ipsum dolor reiciendis
