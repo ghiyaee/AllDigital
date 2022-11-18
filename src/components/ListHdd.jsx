5a0a01654d513c4fc318e18322614bd603e5d1a1
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-11-18 14:22:09 +0330

    update

--- src/components/ListHdd.jsx
+++ src/components/ListHdd.jsx
@@ -17,21 +17,21 @@ const ListHard = () => {
       <div className="wrrap-mobile" key={item.id}>
         <div className="img">
           <img src={item.img} alt="" />
-        </div>
-        <div className="mobile-titel">
-          <p>
-            <span>Brand</span> {item.model}
-          </p>
-          <p>
-            <span>Ram</span> {item.ram}
-          </p>
-          <p>
-            <span>Sdd</span> {item.sdd}
-          </p>
-          <p>
-            <span>price</span> {item.price}$
-          </p>
-          <button onClick={() => addHandel(item)}>Add To Basket</button>
+          <div className="mobile-titel">
+            <p>
+              <span>Brand</span> {item.model}
+            </p>
+            <p>
+              <span>Ram</span> {item.ram}
+            </p>
+            <p>
+              <span>Sdd</span> {item.sdd}
+            </p>
+            <p>
+              <span>price</span> {item.price}$
+            </p>
+            <button onClick={() => addHandel(item)}>Add To Basket</button>
+          </div>
         </div>
       </div>
     );
