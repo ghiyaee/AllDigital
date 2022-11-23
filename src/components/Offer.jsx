73f40492a517332dd024880e4bb3cf4e8c8b6ab3
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-11-24 01:54:24 +0330

    update

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,3 +1,4 @@
+import { logDOM } from "@testing-library/react";
 import { useContext, useState, useEffect } from "react";
 import { Context } from "../context";
 import "../style/offer.scss";
@@ -9,6 +10,10 @@ const Offer = () => {
   let [min_, setMin_] = useState(0);
   let [hr_, setHr_] = useState(10);
   let [active, setActive] = useState(false);
+  let [order, setOrder] = useState("hiden-order");
+  let [order1, setOrder1] = useState("hiden-order");
+   let [order2, setOrder2] = useState("hiden-order");
+  const [isactive,serIsActive]=useState([''])
   
   function dPluse() {
     if (mil_ === 0) {
@@ -49,52 +54,79 @@ const Offer = () => {
         item.count ++
        card.push(item)
         dispatch({ type: "Basket" });
-        
-    }
+        if (item.id === 1) {
+           setOrder((order = "show-order"));
+           setInterval(() => {
+             setOrder((order = "hiden-order"));
+           }, 1500);
+        } 
+        if (item.id === 2) {
+          setOrder1((order1 = "show-order"));
+          setInterval(() => {
+            setOrder1((order1 = "hiden-order"));
+          }, 1500);
+        } 
+          if (item.id === 3) {
+            setOrder2((order2 = "show-order"));
+            setInterval(() => {
+              setOrder2((order2 = "hiden-order"));
+            }, 1500);
+          } 
+        }
+       
   }
-        return (
-          <div className="cradOffer" key={item.id}>
-            <div className="offer-text">
-              <p>offer%</p>
+    return (
+      <div className="cradOffer" key={item.id}>
+        <div className="offer-text">
+          <p>offer%</p>
+        </div>
+        <div className="img-offer">
+          <div className="imgoffer">
+            <img src={item.img} alt="" />
+           
+          </div>
+        </div>
+        <div className="titel-Offer">
+          <div className="titeltext">
+            <div>
+              <p>{item.model}</p>
+              <p>${item.price}</p>
             </div>
-            <div className="img-offer">
-              <div className="imgoffer">
-                <img src={item.img} alt="" />
+            <div className="buying-offer">
+              <div className="arrows">
+                <i className="fa-solid fa-arrow-right"></i>
               </div>
-            </div>
-            <div className="titel-Offer">
-              <div className="titeltext">
-                <div>
-                  <p>{item.model}</p>
-                  <p>${item.price}</p>
-                </div>
-                <div className="buying-offer">
-                  <div className="arrows">
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </div>
-                  <i
-                    className="fa-sharp fa-solid fa-cart-shopping "
-                    onClick={() => addHandel(item)}
-                  ></i>
-                </div>
-              </div>
-              <div className="timeroffer">
-                <span>{hr_}</span>
-                <span>:</span>
-                <span>{min_}</span>
-                <span>:</span>
-                <span>{sec_}</span>
-              </div>
+              <i
+                className="fa-sharp fa-solid fa-cart-shopping "
+                onClick={() => addHandel(item)}
+              ></i>
             </div>
           </div>
-        );
+          <div className="timeroffer">
+            <span>{hr_}</span>
+            <span>:</span>
+            <span>{min_}</span>
+            <span>:</span>
+            <span>{sec_}</span>
+          </div>
+        </div>
+      </div>
+    );
       })
-
   return (
     <div className="container-offer">
-     
-       {render}
-    </div>
+      {render}
+      <div className={`order ${order}`}>
+        <p>Add Order To Basket</p>
+        </div>
+        <div className={`order1 ${order1}`}>
+          <p>Add Order To Basket</p>
+        </div>
+        <div className={`order2 ${order2}`}>
+          <p>Add Order To Basket</p>
+        </div>
+      </div>
+  
   );
 };
 
