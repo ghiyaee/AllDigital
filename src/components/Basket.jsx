53c56693b71bebfafad8c5d7687b75595eb35f41
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-12-01 17:08:39 +0330

    update

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState,useEffect } from "react";
+import { useContext, useState,useEffect} from "react";
 import { Context } from "../context";
 import '../style/basket.scss'
 import img from '../img/imgbas.jpg'
@@ -8,11 +8,11 @@ const Basket = () => {
    let { card, imgbasket } = state;
   let [countPluse, setCountPluse] = useState([]);
   const [total, setTOtal] = useState();
+  const [filter, setFelter] = useState(card)
   const [basketEmpty, setBasketEmpty] = useState([
     { transform: `translateX(-200px)` },
     { transform: `translateX(-100px)` },
   ]);
-  const [filter, setFelter] = useState(card)
   const addHandel = (item) => {
     const ind = filter.indexOf(item);
     const arr = filter;
@@ -39,44 +39,42 @@ const Basket = () => {
   useEffect(() => {
     totalHandel();
   });
+  
   let subTotal = 0;
-  const style={ backgroundImage: `url(${img}) ` }
   const render = filter.map((item) => {
     return (
-          <div className="container-basket" key={item.id} >
-            <div className="wrrap-basket">
-              <div className="basimg">
-                <div className="offer-img">
-                  <img src={item.img} alt="imageProduct" />
-                </div>
-              </div>
-              <div className="sumitem">
-                <div className="text">
-                  <p>{item.model}</p>
-                  <p>${item.price}</p>
-                </div>
-                <p> this item sum:$ {(subTotal = item.price * item.count)}</p>
-                <div className="counter">
-                  <span className="click" onClick={() => addHandel(item)}>
-                    +
-                  </span>
-                  <span>{item.count}</span>
-                  <span
-                    className={`click ${item.count > 1 ? "show" : "hiden"}`}
-                    onClick={() => (item.count === 1 ? "" : minHandel(item, 1))}
-                  >
-                    -
-                  </span>
-                  <span className={`${item.count > 1 ? "hiden" : "show"}`}>
-                    <i
-                      onClick={() => deleHandel(item)}
-                      className="fa-solid fa-trash-can trash "
-                    ></i>
-                  </span>
-                </div>
-              </div>
+      <div className="container-basket" key={item.id}>
+        <div className="wrrap-basket">
+            <div className="offer-img">
+              <img src={item.img} alt="imageProduct" />
             </div>
-          </div >
+          <div className="sumitem">
+            <div className="text">
+              <p>{item.model}</p>
+              <p>${item.price}</p>
+            </div>
+            <p> this item sum:$ {(subTotal = item.price * item.count)}</p>
+            <div className="counter">
+              <span className="click" onClick={() => addHandel(item)}>
+                +
+              </span>
+              <span>{item.count}</span>
+              <span
+                className={`click ${item.count > 1 ? "show" : "hiden"}`}
+                onClick={() => (item.count === 1 ? "" : minHandel(item, 1))}
+              >
+                -
+              </span>
+              <span className={`${item.count > 1 ? "hiden" : "show"}`}>
+                <i
+                  onClick={() => deleHandel(item)}
+                  className="fa-solid fa-trash-can trash "
+                ></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   });
 
